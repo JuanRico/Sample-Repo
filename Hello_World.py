938531cc600fa7b43e3eb2735f2e5238a7296100
Michael DeClue <mdeclue@resonant-tec.com>
2020-09-17 19:25:52 -0500

    Initial Commit

---
+++ Hello_World.py
@@ -0,0 +1,15 @@
+import sys
+
+print("Version: ", sys.version)
+print("Executable: ", sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+name = input("your name? ")
+print(greet("World"))
+print(greet("Mike"))
+print("Hello,", name)
